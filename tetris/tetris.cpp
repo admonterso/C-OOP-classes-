60e0689301be036d2991b1dc7865ab00b2aa0704
Kaxa Gvimradze <kaxagvimraze@gmail.com>
2023-04-05 04:20:04 -0700

    add function move

--- tetris/tetris.cpp
+++ tetris/tetris.cpp
@@ -1,5 +1,32 @@
 #include <ncurses.h>
+int moveCharacter(int& x, int& y, int rows, int columns) {
+    clear();        /* Clear the screen */
+    curs_set(false);
+    
+    /* Draw the character at the current position */
+    mvaddch(y, x, '@');
+    mvprintw((rows - 1), (columns - 50), "rows: %d  columns: %d", rows, columns);
+    mvprintw((rows - 1), (columns - 15), "X: %d  Y: %d", x, y);
+    curs_set(true);
 
+    /* Wait for user input */
+    int ch = getch();
+
+    /* Move the character based on user input */
+    if (ch == KEY_UP && y >= 1) {
+        y--;
+    } else if (ch == KEY_DOWN && y <= rows - 3) {
+        y++;
+    } else if (ch == KEY_LEFT && x >=1) {
+        x--;
+    } else if (ch == KEY_RIGHT && x<columns - 1) {
+        x++;
+    }
+    else if (ch == 27) {
+        return 1;
+    }
+
+}
 int main() {
     int x = 0, y = 0;
     int ch;
@@ -9,31 +36,13 @@ int main() {
     keypad(stdscr, TRUE);/* Enable arrow keys */
     getmaxyx(stdscr, rows, columns);
     while (1) {
-        clear();        /* Clear the screen */
-        curs_set(false);
-        /* Draw the character at the current position */
-        mvaddch(y, x, '@');
-        mvprintw((rows - 1), (columns - 50), "rows: %d  columns: %d", rows, columns);
+    
+    int Move = moveCharacter(x, y, rows, columns);
 
-        mvprintw((rows - 1), (columns - 15), "X: %d  Y: %d", x, y);
-        curs_set(true);
-
-        /* Wait for user input */
-        ch = getch();
-
-        /* Move the character based on user input */
-            if (ch == KEY_UP && y >= 1) {
-                y--;
-            } else if (ch == KEY_DOWN && y <= rows - 3) {
-                y++;
-            } else if (ch == KEY_LEFT && x >=1) {
-                x--;
-            } else if (ch == KEY_RIGHT && x<columns - 1) {
-                x++;
-            } else if (ch == 27) { /* Exit if user presses 'Esc' key */
-                goto END;
-            }
+    if (Move == 1) { /* Exit if user presses 'Esc' key */
+        goto END;
     }
+}
 
 END:
     endwin();           /* End curses mode */
